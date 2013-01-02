3dd0f07a4d83640ccb2d69b3565a9b0d1bb9dc3c
stevdrey <stevdrey@gmail.com>
2013-01-01 20:41:48 -0600

    Se agrega la clase: "Main"

---
+++ pycalc/src/Main.py
@@ -0,0 +1,11 @@
+'''
+Created on 01/01/2013
+
+@author: Steven Rey Sequeira
+'''
+from PyCalc import PyCalc
+
+if __name__ == '__main__':
+    calc= PyCalc()
+    calc.calcular(calc.printMenu())
+    
